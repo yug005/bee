43691e978249a799c746858fed79dedb2a84917a
yug005 <yuga6627@gmail.com>
2025-07-01 14:49:34 +0530

    fs

--- LEC 1/app.js
+++ LEC 1/app.js
@@ -13,4 +13,15 @@ function divide(a, b) {
   }
   return a / b;
 }
+const fs = require("fs");
+
+fs.readFile("demo.txt", "utf-8", (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(data);
+});
+
+console.log(add(5, 3));
 console.log("exit");
