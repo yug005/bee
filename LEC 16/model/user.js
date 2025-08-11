80d4505bb205e8395a05810b5a27504d7a31dcc0
yug005 <yuga6627@gmail.com>
2025-08-11 15:48:49 +0530

    new

---
+++ LEC 16/model/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const BlogPost = new Schema({
+ 
+  title: String,
+  body: String,
+  date: Date
+});
+module.exports=mongoose.model('Blog', BlogPost);
